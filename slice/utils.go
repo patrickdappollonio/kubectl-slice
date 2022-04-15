32a83574bcd7f837f39db56eb45b406923fe29a4
Patrick D'appollonio <patrick@dappollonio.us>
2022-04-15 00:29:51 -0400

    Fix windows piping of stdin. (#44)
    * Fix windows piping of stdin. Fixes #41
    
    * Add support to repoint stdout and stderr to custom writers. Add global test.
    
    * Enable verbose tests.

--- slice/utils.go
+++ slice/utils.go
@@ -14,9 +14,9 @@ func loadfile(fp string) (*bytes.Buffer, error) {
 		return nil, fmt.Errorf("unable to get path to file %q: %s", fp, err.Error())
 	}
 
-	f, err := os.Open(tmplfile)
+	f, err := getFile(tmplfile)
 	if err != nil {
-		return nil, fmt.Errorf("unable to open file %q: %s", fp, err.Error())
+		return nil, err
 	}
 
 	defer f.Close()
@@ -27,4 +27,20 @@ func loadfile(fp string) (*bytes.Buffer, error) {
 	}
 
 	return &buf, nil
+}
+
+func getFile(fp string) (*os.File, error) {
+	if fp == os.Stdin.Name() {
+		// On Windows, the name in Go for stdin is `/dev/stdin` which doesn't
+		// exist. It must use the syscall to point to the file and open it
+		return os.Stdin, nil
+	}
+
+	// Any other file that's not stdin can be opened normally
+	f, err := os.Open(fp)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open file %q: %s", fp, err.Error())
+	}
+
+	return f, nil
 }

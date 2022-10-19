a7aa511614a4e1f420bc785a92bf83bfc30ba270
Douglas Mendez <douglasmakey@gmail.com>
2022-10-19 00:07:21 -0400

    feat(app): Implement include and exclude option - close #22.

--- slice/process.go
+++ slice/process.go
@@ -49,49 +49,35 @@ func (s *Split) parseYAMLManifest(contents []byte) (yamlFile, error) {
 
 	// Check before handling if we're about to filter resources
 	var (
-		hasKindIncluded = len(s.opts.IncludedKinds) > 0
-		hasKindExcluded = len(s.opts.ExcludedKinds) > 0
-		hasNameIncluded = len(s.opts.IncludedNames) > 0
-		hasNameExcluded = len(s.opts.ExcludedNames) > 0
+		hasIncluded = len(s.opts.Included) > 0
+		hasExcluded = len(s.opts.Excluded) > 0
 	)
 
 	s.log.Printf(
-		"Applying filters -> kindIncluded: %v; kindExcluded: %v; nameIncluded: %v; nameExcluded: %v",
-		s.opts.IncludedKinds, s.opts.ExcludedKinds, s.opts.IncludedNames, s.opts.ExcludedNames,
-	)
+		"Applying filters -> Included: %v; Excluded: %v", s.opts.Included, s.opts.Excluded)
 
 	s.log.Printf("Found K8s meta -> %#v", k8smeta)
 
 	// Check if we have a Kubernetes kind and we're requesting inclusion or exclusion
-	if k8smeta.Kind == "" && (hasKindIncluded || hasKindExcluded) {
+	if k8smeta.Kind == "" && (hasIncluded || hasExcluded) {
 		return yamlFile{}, fmt.Errorf("unable to find Kubernetes \"kind\" field in file number %d", s.fileCount)
 	}
 
 	// Check if we have a Kubernetes name and we're requesting inclusion or exclusion
-	if k8smeta.Name == "" && (hasNameIncluded || hasNameExcluded) {
+	if k8smeta.Name == "" && (hasIncluded || hasExcluded) {
 		return yamlFile{}, fmt.Errorf("unable to find Kubernetes \"metadata.name\" field in file number %d", s.fileCount)
 	}
 
-	// We need to check if the file is skipped by kind
-	if hasKindIncluded || hasKindExcluded || hasNameIncluded || hasNameExcluded {
-		// If we're working with including only specific kinds, then filter by it
-		if hasKindIncluded && !inSliceIgnoreCaseGlob(s.opts.IncludedKinds, k8smeta.Kind) {
-			return yamlFile{}, &skipErr{kind: "kind", name: k8smeta.Kind}
+	// We need to check if the file should be skipped
+	if hasExcluded || hasIncluded {
+		// If we're working with including only specific resources, then filter by them
+		if hasIncluded && !inSliceIgnoreCaseGlob(s.opts.Included, fmt.Sprintf("%s/%s", k8smeta.Kind, k8smeta.Name)) {
+			return yamlFile{}, &skipErr{kind: "kind/name", name: fmt.Sprintf("%s/%s", k8smeta.Kind, k8smeta.Name)}
 		}
 
-		// Otherwise exclude kinds based on the parameter received
-		if hasKindExcluded && inSliceIgnoreCaseGlob(s.opts.ExcludedKinds, k8smeta.Kind) {
-			return yamlFile{}, &skipErr{kind: "kind", name: k8smeta.Kind}
-		}
-
-		// If we're working with including only specific names, then filter by it
-		if hasNameIncluded && !inSliceIgnoreCaseGlob(s.opts.IncludedNames, k8smeta.Name) {
-			return yamlFile{}, &skipErr{kind: "name", name: k8smeta.Name}
-		}
-
-		// Otherwise exclude names based on the parameter received
-		if hasNameExcluded && inSliceIgnoreCaseGlob(s.opts.ExcludedNames, k8smeta.Name) {
-			return yamlFile{}, &skipErr{kind: "name", name: k8smeta.Name}
+		// Otherwise exclude resources based on the parameter received
+		if hasExcluded && inSliceIgnoreCaseGlob(s.opts.Excluded, fmt.Sprintf("%s/%s", k8smeta.Kind, k8smeta.Name)) {
+			return yamlFile{}, &skipErr{kind: "kind/name", name: fmt.Sprintf("%s/%s", k8smeta.Kind, k8smeta.Name)}
 		}
 	}
 
--- slice/validate.go
+++ slice/validate.go
@@ -2,17 +2,12 @@ package slice
 
 import (
 	"fmt"
+	"regexp"
 )
 
+const regKN = `^[^/]+/[^/]+$`
+
 func (s *Split) init() error {
-	if len(s.opts.IncludedKinds) > 0 && len(s.opts.ExcludedKinds) > 0 {
-		return fmt.Errorf("cannot specify both included and excluded kinds")
-	}
-
-	if len(s.opts.IncludedNames) > 0 && len(s.opts.ExcludedNames) > 0 {
-		return fmt.Errorf("cannot specify both included and excluded names")
-	}
-
 	s.log.Printf("Loading file %s", s.opts.InputFile)
 	buf, err := loadfile(s.opts.InputFile)
 	if err != nil {
@@ -33,6 +28,56 @@ func (s *Split) init() error {
 
 	if err := s.compileTemplate(); err != nil {
 		return err
+	}
+
+	if err := s.validateFilters(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Split) validateFilters() error {
+	if len(s.opts.IncludedKinds) > 0 && len(s.opts.ExcludedKinds) > 0 {
+		return fmt.Errorf("cannot specify both included and excluded kinds")
+	}
+
+	if len(s.opts.IncludedNames) > 0 && len(s.opts.ExcludedNames) > 0 {
+		return fmt.Errorf("cannot specify both included and excluded names")
+	}
+
+	if len(s.opts.Included) > 0 && len(s.opts.Excluded) > 0 {
+		return fmt.Errorf("cannot specify both included and excluded")
+	}
+
+	// Merge all filters into excluded and included.
+	for _, v := range s.opts.IncludedKinds {
+		s.opts.Included = append(s.opts.Included, fmt.Sprintf("%s/*", v))
+	}
+
+	for _, v := range s.opts.ExcludedKinds {
+		s.opts.Excluded = append(s.opts.Excluded, fmt.Sprintf("%s/*", v))
+	}
+
+	for _, v := range s.opts.IncludedNames {
+		s.opts.Included = append(s.opts.Included, fmt.Sprintf("*/%s", v))
+	}
+
+	for _, v := range s.opts.ExcludedNames {
+		s.opts.Excluded = append(s.opts.Excluded, fmt.Sprintf("*/%s", v))
+	}
+
+	// Validate included and excluded filters.
+	for _, included := range s.opts.Included {
+		if matched, _ := regexp.MatchString(regKN, included); !matched {
+			return fmt.Errorf("invalidad included pattern '%s' should be {kind}/${name}", included)
+		}
+	}
+
+	for _, excluded := range s.opts.Excluded {
+		if matched, _ := regexp.MatchString(regKN, excluded); !matched {
+			return fmt.Errorf("invalidad excluded pattern '%s' should be {kind}/${name}", excluded)
+		}
 	}
 
 	return nil
